eaa86f4ffbad991fca876cd9a437793d0623aa49
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-22 17:23:24 +0300

    Hide additional notes by default Add show more and show less buttons Update question-listing.js file - Hide and show note functionality

--- app/components/question-listing.js
+++ app/components/question-listing.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isNoteShowing: false,
+  actions: {
+    showNote: function() {
+      this.set("isNoteShowing", true)
+    },
+    hideNote: function() {
+      this.set("isNoteShowing", false)
+    }
+  }
 });
