913c0887f77d22154976c0e890bdb2db5c334d83
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-24 22:44:55 +0300

    Update Question Functionality

--- app/components/question-details.js
+++ app/components/question-details.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
       if (confirm("Are you sure you want to delete this question?")) {
         this.sendAction('removeQuestion', question);
       }
+    },
+    update(question, params) {
+      this.sendAction('update', question, params)
     }
   }
 });
--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isEditQuestion: false,
+  actions: {
+    isEditQuestion() {
+      this.set("isEditQuestion", true);
+    },
+    update(question) {
+      var params = {
+        topic: this.get('topic'),
+        title: this.get('title'),
+        author: this.get('author'),
+        additionalNotes: this.get('additionalNotes')
+      };
+      this.set("isEditQuestion", false);
+      this.sendAction("saveQuestions", question, params);
+    }
+  }
 });
