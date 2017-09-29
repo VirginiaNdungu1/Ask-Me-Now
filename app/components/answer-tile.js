fd99a767a0cba3765730e37bf1bae4dc569d8624
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-09-29 16:10:23 +0300

    Hide and Show Edit ANswer Form

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isEditAnswer: false,
+  actions: {
+    isEditAnswer() {
+      this.set("isEditAnswer", true);
+    },
+  }
 });
